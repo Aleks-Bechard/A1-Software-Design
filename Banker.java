3f34dad8e99785fbe3cb26b8f4ea8ad5f8e86ab6
aleks bechard <97978712+Aleks-Bechard@users.noreply.github.com>
2022-03-05 17:16:42 -0500

    Add files via upload

---
+++ Banker.java
@@ -0,0 +1,40 @@
+import java.util.Random;
+/**
+ * Nupur Patel
+ */
+public class Banker
+{
+
+   protected int score;
+   protected Suits suit;
+
+   public void createBankerHand ()
+   {
+      Random random = new Random();
+
+      for (int i = 0; i < 2; i++) {
+         System.out.print(Value.values()[random.nextInt(Value.values().length)]);
+         System.out.println(" " + Suits.values()[random.nextInt(Suits.values().length)]);
+      }
+   }
+
+   public void setScore (int score) throws Exception
+   {
+      if (score < 0) {
+         throw new Exception("You need to enter the score greater than 0!!");
+      }
+      this.score = score;
+   }
+
+   public int getScore ()
+   {
+      return score;
+   }
+
+   public Suits getSuit ()
+   {
+      return suit;
+   }
+
+
+}
